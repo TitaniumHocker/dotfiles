aad54e8d4e38991c5ed0c32cc370c0bf51809bf6
Ivan Fedorov <inbox@titaniumhocker.ru>
2022-05-03 18:36:14 +0300

    Fixed intendation for C filetype

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -1,6 +1,9 @@
 local loaded, lsp = pcall(require, "lspconfig")
 if not loaded then return end
 
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
 lsp.clangd.setup {}
 
 -- Ensure that language server is attached to buffer
