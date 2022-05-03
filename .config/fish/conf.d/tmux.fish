f5c3faf12a0f56a4fa3df0a89e1dabdd7db90331
Ivan Fedorov <inbox@titaniumhocker.ru>
2022-05-03 18:35:36 +0300

    Added term type fix for tmux

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -1,3 +1,7 @@
 if status is-interactive && command -v tmux >/dev/null 2>&1 && [ -d "$HOME/.tmux" ]
     tmux new -As S >/dev/null 2>&1
 end
+
+if [ "$TERM" = "tmux-256color" ]
+    set --global --export TERM "screen-256color"
+end
